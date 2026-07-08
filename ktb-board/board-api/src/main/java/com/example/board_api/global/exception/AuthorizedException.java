6422fed8cec2c59d48462a60780816b1dd3657b1
jerry7129 <jerry7129@naver.com>
2026-07-08 17:48:43 +0900

    Fix directory structure: remove nested folders and move to root

--- ktb-board/board-api/src/main/java/com/example/board_api/global/exception/AuthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.example.board_api.global.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class AuthorizedException extends BusinessException {
-    public AuthorizedException(String code) {
-        super(code, "인증되지 않은 요청입니다.", HttpStatus.UNAUTHORIZED);
-    }
-}
--- ktb-board/board-api/src/main/java/com/example/board_api/global/util/converter/EnumMapperType.java
+++
@@ -1,5 +0,0 @@
-package com.example.board_api.global.util.converter;
-
-public interface EnumMapperType {
-    Integer getLegacyCode();
-}
